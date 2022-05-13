c3cc1dd1dd69ce8d822e4845eaf735b09992c945
Priya Wadhwa <priya@chainguard.dev>
2022-05-13 14:00:54 -0700

    Additional logging
    Signed-off-by: Priya Wadhwa <priya@chainguard.dev>

--- cmd/prober/prober.go
+++ cmd/prober/prober.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The Sigstore Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -87,6 +101,7 @@ func observeRequest(host string, r ReadProberCheck) error {
 		hostLabel:       host,
 	}
 	fmt.Println("Status code: ", resp.StatusCode)
+	fmt.Println("Latency: ", latency)
 	endpointLatenciesHistogram.With(labels).Observe(float64(latency))
 	endpointLatenciesSummary.With(labels).Observe(float64(latency))
 	return nil

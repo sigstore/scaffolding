c641b2f0257d1a0f5d678c61677d93f2037359a6
Ville Aikas <vaikas@chainguard.dev>
2022-07-30 16:44:40 -0700

    Create a secret that's used by the tuf server. DOES NOT WORK yet.
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

---
+++ cmd/tuf/createsecret/main.go
@@ -0,0 +1,159 @@
+// Copyright 2022 The Sigstore Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"os"
+
+	"github.com/sigstore/cosign/cmd/cosign/cli/rekor"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"knative.dev/pkg/logging"
+	"knative.dev/pkg/signals"
+	"sigs.k8s.io/release-utils/version"
+)
+
+const (
+	// These are the keys held in the secrets
+	fulcioSecretKey = "cert"
+	ctSecretKey     = "public"
+
+	// These are the fields we create in the secret
+	fulcioSecretKeyOut = "fulcio-cert"
+	ctSecretKeyOut     = "ct-pubkey"
+	rekorSecretKeyOut  = "rekor-pubkey"
+)
+
+var (
+	fulcioSecret = flag.String("fulcio-secret", "fulcio-secret", "Secret holding Fulcio cert")
+	// URL to Rekor to query for the Rekor public key to include in the trust root.
+	// Could replace with rekor-pubkey, if that can be resolved early enough (don't know)
+	rekorURL = flag.String("rekor-url", "http://rekor.rekor-system.svc", "Address of the Rekor server")
+	// TODO(vaikas): Consider creating this secret possibly.
+	/*
+		rekorSecret   = flag.String("rekor-secret", "fulcio-secrets", "Secret holding Fulcio cert")
+	*/
+	ctlogSecret = flag.String("ctlog-secret", "ctlog-public-key", "Secret holding CTLog public key")
+
+	secretName = flag.String("secret-name", "tuf-secrets", "Name of the secret to create holding necessary information to create/run tuf service")
+)
+
+func main() {
+	flag.Parse()
+	ns := os.Getenv("NAMESPACE")
+	if ns == "" {
+		panic("env variable NAMESPACE must be set")
+	}
+	ctx := signals.NewContext()
+
+	versionInfo := version.GetVersionInfo()
+	logging.FromContext(ctx).Infof("running create_secrets Version: %s GitCommit: %s BuildDate: %s", versionInfo.GitVersion, versionInfo.GitCommit, versionInfo.BuildDate)
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Failed to get InClusterConfig: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Failed to get clientset: %v", err)
+	}
+
+	// Grab the Fulcio root cert
+	nsSecrets := clientset.CoreV1().Secrets(ns)
+	fs, err := nsSecrets.Get(ctx, *fulcioSecret, metav1.GetOptions{})
+	if err != nil && !apierrs.IsNotFound(err) {
+		logging.FromContext(ctx).Panicf("Failed to get secret %s/%s: %v", ns, *fulcioSecret, err)
+	}
+	fCert := fs.Data[fulcioSecretKey]
+	if fCert == nil || len(fCert) == 0 {
+		logging.FromContext(ctx).Panicf("Fulcio cert key %q is missing %s/%s", fulcioSecretKey, ns, *fulcioSecret)
+	}
+	// Grab the  ctlog public key
+	cs, err := nsSecrets.Get(ctx, *ctlogSecret, metav1.GetOptions{})
+	if err != nil && !apierrs.IsNotFound(err) {
+		logging.FromContext(ctx).Panicf("Failed to get secret %s/%s: %v", ns, *ctlogSecret, err)
+	}
+	cPub := cs.Data[ctSecretKey]
+	if cPub == nil || len(cPub) == 0 {
+		logging.FromContext(ctx).Panicf("Ctlog public key %q is missing %s/%s", ctSecretKey, ns, *ctlogSecret)
+	}
+
+	// And finally grab the Rekor data from the server.
+	rekorClient, err := rekor.NewClient(*rekorURL)
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Unable to get rekor client: %v", err)
+	}
+	rekorKey, err := rekorClient.Pubkey.GetPublicKey(nil)
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Unable to fetch rkeor key: %v", err)
+	}
+
+	data := make(map[string][]byte)
+	data[fulcioSecretKeyOut] = fCert
+	data[ctSecretKeyOut] = cPub
+	data[rekorSecretKeyOut] = []byte(rekorKey.Payload)
+
+	// See if there's an existing secret first
+	existingSecret, err := nsSecrets.Get(ctx, *secretName, metav1.GetOptions{})
+	if err != nil && !apierrs.IsNotFound(err) {
+		logging.FromContext(ctx).Panicf("Failed to get secret %s/%s: %v", ns, *secretName, err)
+	}
+
+	// If we found the secret, just make sure all the fields are there.
+	if err == nil && existingSecret != nil && existingSecret.Data != nil {
+		esd := existingSecret.Data
+		update := false
+		if bytes.Compare(esd[fulcioSecretKeyOut], data[fulcioSecretKeyOut]) != 0 {
+			logging.FromContext(ctx).Infof("Fulcio key missing or different than expected, updating")
+			update = true
+		}
+		if bytes.Compare(esd[ctSecretKeyOut], data[ctSecretKeyOut]) != 0 {
+			logging.FromContext(ctx).Infof("CTLog key missing or different than expected, updating")
+		}
+		if bytes.Compare(esd[rekorSecretKeyOut], data[rekorSecretKeyOut]) != 0 {
+			logging.FromContext(ctx).Infof("Rekor key missing or different than expected, updating")
+		}
+		if !update {
+			logging.FromContext(ctx).Infof("Found existing secret config with all the expected keys")
+			return
+		}
+		existingSecret.Data = data
+		_, err = nsSecrets.Update(ctx, existingSecret, metav1.UpdateOptions{})
+		if err != nil {
+			logging.FromContext(ctx).Fatalf("Failed to update secret %s/%s: %v", ns, *secretName, err)
+		}
+		logging.FromContext(ctx).Infof("Updated secret %s/%s", ns, *secretName)
+		return
+	}
+
+	// Secret is not there, so just create id.
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      *secretName,
+		},
+		Data: data,
+	}
+	_, err = nsSecrets.Create(ctx, secret, metav1.CreateOptions{})
+	if err != nil {
+		logging.FromContext(ctx).Fatalf("Failed to create secret %s/%s: %v", ns, *secretName, err)
+	}
+	logging.FromContext(ctx).Infof("Created secret %s/%s", ns, *secretName)
+}
---
+++ hack/setup-scaffolding.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Copyright 2022 The Sigstore Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Because setting up tuf requires peeking into several namespaces, we can't
+# do that simply from a Job because a pod can't read secrets in the other
+# namespaces that we need.
+# Alternative would be to create a full blown controller and reconcile the
+# secret from there, which might be something that we might consider doing
+# anyways.
+# But for now, trying to make forward progress here, we have to run this
+# script _after_ creating the scaffolding and once it's up.
+
+# So, after [this](https://github.com/sigstore/scaffolding/blob/main/getting-started.md#then-wait-for-the-jobs-that-setup-dependencies-to-finish) step completes, run this script.
+
+# First create the job which will combine a tuf-system/tuf-secrets secret
+# that holds all the information that the tuf server will need to create
+# the tuf store.
+ko apply -f ./config/tuf/createsecret
+
+# Then wait for it to do it's job
+kubectl wait --timeout=5m -n tuf-system --for=condition=Complete jobs createsecret
+
+# Copy the necessary secrets to the tuf-system namespace
+# TODO(vaikas): Just copy the bits we care about
+kubectl -n ctlog-system get secrets ctlog-public-key -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
+kubectl -n fulcio-system get secrets fulcio-secret -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
+
+# Then launch the tuf server
+ko apply -BRf ./config/tuf/server
+
+# Above however is currently failing with the following error, and I'm not
+# entirely sure how to fix, so I'm just creating this as a checkpoint.
+```
+vaikas@villes-mbp scaffolding % kubectl -n tuf-system logs tuf-00003-deployment-57f574c94c-kgbhr tuf
+{"level":"info","ts":1659224120.0725565,"logger":"fallback","caller":"createrepo/main.go:57","msg":"running create_repo Version: devel GitCommit: 46ebe1479b33e5e0ba1efd0a6d967a68d1bbab5a BuildDate: 2022-07-27T23:51:17"}
+{"level":"info","ts":1659224120.1282668,"logger":"fallback","caller":"createrepo/main.go:148","msg":"Creating the FS in \"/tmp\""}
+{"level":"info","ts":1659224120.1304667,"logger":"fallback","caller":"createrepo/main.go:152","msg":"Creating new repo in \"/tmp\""}
+{"level":"error","ts":1659224120.3686304,"logger":"fallback","caller":"createrepo/main.go:191","msg":"Failed to SnashotWithExpires tuf: missing metadata file targets.json","stacktrace":"main.createRepo\n\tgithub.com/sigstore/scaffolding/cmd/tuf/createrepo/main.go:191\nmain.main\n\tgithub.com/sigstore/scaffolding/cmd/tuf/createrepo/main.go:85\nruntime.main\n\truntime/proc.go:250"}
+{"level":"panic","ts":1659224120.369749,"logger":"fallback","caller":"createrepo/main.go:87","msg":"Creating repot: tuf: missing metadata file targets.json","stacktrace":"main.main\n\tgithub.com/sigstore/scaffolding/cmd/tuf/createrepo/main.go:87\nruntime.main\n\truntime/proc.go:250"}
+panic: Creating repot: tuf: missing metadata file targets.json
+```

384460157aa507c3e272a354dc2214b7800b57d0
Ville Aikas <11279988+vaikas@users.noreply.github.com>
2022-11-28 14:31:13 -0800

    Exercise TSA in action/release tests. (#484)
    * it's >0.4.12 not >=0.4.12 for grpc-fulcio.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    * Exercise TSA now that it's in the release.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    * address PR feedback.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    * argh, tuf-system does not exist yet. move copying secrets to after it is created.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    * go with 0.5.1 tests.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    * fix typo. get ksvc insteace of tsa.
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>
    
    Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

--- hack/setup-scaffolding-from-release.sh
+++ hack/setup-scaffolding-from-release.sh
@@ -101,7 +101,7 @@ echo '::group:: Wait for Fulcio ready'
 kubectl wait --timeout 5m -n fulcio-system --for=condition=Complete jobs --all
 kubectl wait --timeout 5m -n fulcio-system --for=condition=Ready ksvc fulcio
 # this checks if the requested version is > 0.4.12 (and therefore has fulcio-grpc in it)
-if [ "${PATCH}" -ge 12 ] || [ "${MINOR}" -ge 5 ]; then
+if [ "${PATCH}" -gt 12 ] || [ "${MINOR}" -ge 5 ]; then
   kubectl wait --timeout 5m -n fulcio-system --for=condition=Ready ksvc fulcio-grpc
 fi
 echo '::endgroup::'
@@ -132,6 +132,10 @@ kubectl apply -f "${TUF}"
 kubectl -n ctlog-system get secrets ctlog-public-key -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
 kubectl -n fulcio-system get secrets fulcio-pub-key -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
 kubectl -n rekor-system get secrets rekor-pub-key -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
+
+if [ "${INSTALL_TSA}" == "true" ]; then
+kubectl -n tsa-system get secrets tsa-cert-chain -oyaml | sed 's/namespace: .*/namespace: tuf-system/' | kubectl apply -f -
+fi
 echo '::endgroup::'
 
 if [ "${INSTALL_TSA}" == "true" ]; then

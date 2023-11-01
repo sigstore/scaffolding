b376412eb51d48280b14a03c6c1389e7779e9e3b
Bob Callaway <bcallaway@google.com>
2023-11-01 08:49:40 -0400

    upgrade default & add new k8s versions to improve coverage
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- hack/setup-kind.sh
+++ hack/setup-kind.sh
@@ -33,8 +33,8 @@ do
 done
 
 # Defaults
-K8S_VERSION="v1.24.x"
-KNATIVE_VERSION="1.6.0"
+K8S_VERSION="v1.25.x"
+KNATIVE_VERSION="1.8.0"
 REGISTRY_NAME="registry.local"
 REGISTRY_PORT="5001"
 CLUSTER_SUFFIX="cluster.local"

9d7c4930f4cf40168b1befaa57bf8bdb4dfd1c13
Bob Callaway <bcallaway@google.com>
2024-01-10 09:55:23 -0500

    bump terraform to 1.6.6 and all google-beta modules to 5.10.0
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- terraform/gcp/modules/gke_cluster/versions.tf
+++ terraform/gcp/modules/gke_cluster/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = "1.6.5"
+  required_version = "1.6.6"
 
   required_providers {
     google = {
@@ -23,7 +23,7 @@ terraform {
       source  = "hashicorp/google"
     }
     google-beta = {
-      version = "5.8.0"
+      version = "5.10.0"
       source  = "hashicorp/google-beta"
     }
     random = {

3f8559c8298dcfaee185a041abdffab92dbf09d3
Bob Callaway <bobcallaway@users.noreply.github.com>
2024-06-14 23:50:05 -0400

    bump google provider to 5.33.0 (#1137)
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- terraform/gcp/modules/monitoring/rekor/versions.tf
+++ terraform/gcp/modules/monitoring/rekor/versions.tf
@@ -19,7 +19,7 @@ terraform {
 
   required_providers {
     google = {
-      version = "5.29.1"
+      version = "5.33.0"
       source  = "hashicorp/google"
     }
   }
--- terraform/gcp/modules/sigstore/versions.tf
+++ terraform/gcp/modules/sigstore/versions.tf
@@ -19,11 +19,11 @@ terraform {
 
   required_providers {
     google = {
-      version = "5.29.1"
+      version = "5.33.0"
       source  = "hashicorp/google"
     }
     google-beta = {
-      version = "5.29.1"
+      version = "5.33.0"
       source  = "hashicorp/google-beta"
     }
   }

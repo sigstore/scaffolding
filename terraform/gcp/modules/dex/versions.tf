c29ccab4efdd75617d9818d0320ddf73913e29b9
Bob Callaway <bcallaway@google.com>
2024-12-18 09:20:31 -0500

    bump terraform version and provider to latest
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- terraform/gcp/modules/dex/versions.tf
+++ terraform/gcp/modules/dex/versions.tf
@@ -15,11 +15,11 @@
  */
 
 terraform {
-  required_version = "1.10.0"
+  required_version = "1.10.2"
 
   required_providers {
     google = {
-      version = "6.12.0"
+      version = "6.14.0"
       source  = "hashicorp/google"
     }
   }

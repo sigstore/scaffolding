3388988b4a9e9b041469c0b80fa891cf93c38313
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-04-10 21:49:26 +0000

    set env iff github actions
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- actions/test_containers/run-containers.sh
+++ actions/test_containers/run-containers.sh
@@ -89,11 +89,12 @@ for repo in rekor fulcio timestamp-authority; do
     popd
 done
 
-# GitHub action env and outputs
+if [[ -n "$GITHUB_ACTIONS" ]]; then
+  # GitHub action env and outputs
+  echo "OIDC_URL=$(echo $OIDC_URL)" >> "$GITHUB_ENV"
+  echo "oidc-url=$(echo $OIDC_URL)" >> "$GITHUB_OUTPUT"
 
-echo "OIDC_URL=$(echo $OIDC_URL)" >> "$GITHUB_ENV"
-echo "oidc-url=$(echo $OIDC_URL)" >> "$GITHUB_OUTPUT"
-
-export TSA_URL="http://$(hostname):3004"
-echo "TSA_URL=$(echo $TSA_URL)" >> "$GITHUB_ENV"
-echo "tsa-url=$(echo $TSA_URL)" >> "$GITHUB_OUTPUT"
+  export TSA_URL="http://$(hostname):3004"
+  echo "TSA_URL=$(echo $TSA_URL)" >> "$GITHUB_ENV"
+  echo "tsa-url=$(echo $TSA_URL)" >> "$GITHUB_OUTPUT"
+fi

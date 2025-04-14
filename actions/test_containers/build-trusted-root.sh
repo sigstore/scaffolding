8a386476a467aa30c7e02af45a1aa2c49fd4e493
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-04-14 23:55:06 +0000

    unbound var
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- actions/test_containers/build-trusted-root.sh
+++ actions/test_containers/build-trusted-root.sh
@@ -106,6 +106,7 @@ echo "Wrote trusted_root.json & signing_config.json"
 CWD="$(pwd)"
 export TRUSTED_ROOT="$CWD/trusted_root.json"
 export SIGNING_CONFIG="$CWD/signing_config.json"
+GITHUB_ACTIONS="${GITHUB_ACTIONS:-}"
 if [[ -n "$GITHUB_ACTIONS" ]]; then
   # GitHub action env and outputs
   echo "TRUSTED_ROOT=$TRUSTED_ROOT" >> "$GITHUB_ENV"

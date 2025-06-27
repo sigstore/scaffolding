dcde4a8dab89e59916e46b6da503da3139d0cc3c
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-06-27 17:48:31 -0400

    fetch the last tile and entry
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- cmd/prober/rekor_v2.go
+++ cmd/prober/rekor_v2.go
@@ -25,29 +25,27 @@ import (
 	"github.com/transparency-dev/tessera/api/layout"
 )
 
-// checkRekorV2CompleteFirstTile checks to see if the first tile in a shard is still a partial tile.
-// If no, then we must fetch a partial tile. If yes, we must fetch a full tile.
-// See https://github.com/C2SP/C2SP/issues/145.
-func checkRekorV2CompleteFirstTile(rekorURL string) (bool, error) {
-	req, err := retryablehttp.NewRequest("GET", rekorURL+"/api/v2/checkpoint", nil)
+// getTreeSize returns the size of the rekorV2 log tree.
+func getTreeSize(rekorV2URL string) (int, error) {
+	req, err := retryablehttp.NewRequest("GET", rekorV2URL+"/api/v2/checkpoint", nil)
 	if err != nil {
-		return false, fmt.Errorf("invalid request for checkpoint: %w", err)
+		return 0, fmt.Errorf("invalid request for checkpoint: %w", err)
 	}
 
 	setHeaders(req, "", ReadProberCheck{})
 	resp, err := retryableClient.Do(req)
 	if err != nil {
-		return false, fmt.Errorf("unexpected error getting loginfo endpoint: %w", err)
+		return 0, fmt.Errorf("unexpected error getting loginfo endpoint: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return false, fmt.Errorf("unexpected response code received from loginfo endpoint: %w", err)
+		return 0, fmt.Errorf("unexpected response code received from loginfo endpoint: %w", err)
 	}
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return false, fmt.Errorf("reading loginfo body: %w", err)
+		return 0, fmt.Errorf("reading loginfo body: %w", err)
 	}
 
 	// The second line will be the log size.
@@ -55,48 +53,36 @@ func checkRekorV2CompleteFirstTile(rekorURL string) (bool, error) {
 	logSizeBytes := bytes.Split(bodyBytes, []byte{'\n'})[1]
 	logSize, err := strconv.Atoi(string(logSizeBytes))
 	if err != nil {
-		return false, fmt.Errorf("parsing log size: %w", err)
+		return 0, fmt.Errorf("parsing log size: %w", err)
 	}
-	hasCompleteFirstTile := logSize > layout.TileWidth
-	Logger.Debug(fmt.Sprintf("rekorv2 shard has complete first tile: %t, shard: %s, size: %d", hasCompleteFirstTile, rekorURL, logSize))
-	return hasCompleteFirstTile, nil
+	return logSize, nil
 }
 
 // determineRekorV2ShardCoverage determines which endpoints to check for a given rekorV2 shard host.
 // See https://github.com/sigstore/rekor-tiles/blob/98cd4a77300f81eb79ca50f5b8d70ee2a00cbd50/api/proto/rekor/v2/rekor_service.proto#L74.
-func determineRekorV2ShardCoverage(rekorURL string) ([]*ReadProberCheck, error) {
-	hasCompleteFirtstTile, err := checkRekorV2CompleteFirstTile(rekorURL)
+func determineRekorV2ShardCoverage(rekorV2URL string) ([]*ReadProberCheck, error) {
+	treeSize, err := getTreeSize(rekorV2URL)
 	if err != nil {
 		return nil, err
 	}
+	tileIndex := treeSize / layout.TileWidth
+	level := 0 // We always want the items at the botton of the tree (leaf nodes).
+	partial := layout.PartialTileSize(uint64(level), uint64(tileIndex), uint64(treeSize))
+	tilePath := layout.TilePath(uint64(level), uint64(tileIndex), partial)
+	entriesPath := layout.EntriesPath(uint64(tileIndex), partial)
 	proberChecks := []*ReadProberCheck{
 		{
 			Endpoint: "/api/v2/checkpoint",
 			Method:   GET,
 		},
-	}
-	if hasCompleteFirtstTile {
-		proberChecks = append(proberChecks,
-			&ReadProberCheck{
-				Endpoint: "/api/v2/tile/entries/000",
-				Method:   GET,
-			},
-			&ReadProberCheck{
-				Endpoint: "/api/v2/tile/0/000",
-				Method:   GET,
-			},
-		)
-	} else {
-		proberChecks = append(proberChecks,
-			&ReadProberCheck{
-				Endpoint: "/api/v2/tile/entries/000.p1/1",
-				Method:   GET,
-			},
-			&ReadProberCheck{
-				Endpoint: "/api/v2/tile/0/000.p1/1",
-				Method:   GET,
-			},
-		)
+		{
+			Endpoint: fmt.Sprintf("/api/v2/%s", tilePath),
+			Method:   GET,
+		},
+		{
+			Endpoint: fmt.Sprintf("/api/v2/%s", entriesPath),
+			Method:   GET,
+		},
 	}
 	return proberChecks, nil
 }

d5a4a63524d9699bad1874f8fd7db42995799445
Colleen Murphy <colleenmurphy@google.com>
2025-11-20 14:21:59 -0800

    Move commands to separate modules
    Make each helper command its own module to avoid maintaining every
    tool's dependencies in a single go.mod.
    
    Also remove update-deps.sh and update-codegen.sh which weren't used
    anywhere.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ tools/create-tink-keyset/go.mod
@@ -0,0 +1,70 @@
+module github.com/sigstore/scaffolding/tools/create-tink-keyset
+
+go 1.25.0
+
+require (
+	github.com/sigstore/rekor-tiles v0.1.11
+	github.com/spf13/cobra v1.10.1
+	github.com/spf13/viper v1.21.0
+	github.com/tink-crypto/tink-go-gcpkms/v2 v2.2.0
+	github.com/tink-crypto/tink-go/v2 v2.5.0
+)
+
+require (
+	cloud.google.com/go/auth v0.17.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
+	cloud.google.com/go/iam v1.5.3 // indirect
+	github.com/aws/aws-sdk-go v1.55.8 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.39.6 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.31.17 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.3 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/go-containerregistry v0.20.6 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.2.0 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/letsencrypt/boulder v0.20251118.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.1-0.20231216201459-8508981c8b6c // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.9.1 // indirect
+	github.com/sigstore/protobuf-specs v0.5.0 // indirect
+	github.com/sigstore/sigstore v1.9.6-0.20250729224751-181c5d3339b3 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/titanous/rocacheck v0.0.0-20171023193734-afe73141d399 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.63.0 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.step.sm/crypto v0.74.0 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/mod v0.30.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/oauth2 v0.32.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/api v0.255.0 // indirect
+	google.golang.org/genproto v0.0.0-20250922171735-9219d122eba9 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20251022142026-3a174f9686a8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+	google.golang.org/grpc v1.77.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
---
+++ tools/fulcio/cmd/fulcio/createcerts/main.go
@@ -0,0 +1,177 @@
+// Copyright 2022 The Sigstore Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"math"
+	"math/big"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/sigstore/scaffolding/tools/secret/pkg/secret"
+	"go.step.sm/crypto/pemutil"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"knative.dev/pkg/logging"
+	"knative.dev/pkg/signals"
+	"sigs.k8s.io/release-utils/version"
+)
+
+var (
+	secretName       = flag.String("secret", "fulcio-secrets", "Name of the secret to create for the certs")
+	pubkeySecretName = flag.String("pubkeysecret", "fulcio-pub-key", "Name of the secret that holds the public Fulcio information like cert / public key")
+	certOrg          = flag.String("cert-organization", "Linux Foundation", "Name of the organization for certificate creation")
+	certCountry      = flag.String("cert-country", "USA", "Name of the country for certificate creation")
+	certProvince     = flag.String("cert-province", "California", "Name of the province for certificate creation")
+	certLocality     = flag.String("cert-locality", "San Francisco", "Name of the locality for certificate creation")
+	certAddr         = flag.String("cert-address", "548 Market St", "Name of the address for certificate creation")
+	certPostal       = flag.String("cert-postal", "57274", "Name of the postal code for certificate creation")
+)
+
+func main() {
+	flag.Parse()
+
+	ns := os.Getenv("NAMESPACE")
+	if ns == "" {
+		panic("env variable NAMESPACE must be set")
+	}
+	ctx := signals.NewContext()
+
+	versionInfo := version.GetVersionInfo()
+	logging.FromContext(ctx).Infof("running create_certs Version: %s GitCommit: %s BuildDate: %s", versionInfo.GitVersion, versionInfo.GitCommit, versionInfo.BuildDate)
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Failed to get InClusterConfig: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Failed to get clientset: %v", err)
+	}
+
+	// Just create the cert always in case we need to update or create it.
+	certPEM, pubPEM, privPEM, pwd, err := createAll()
+	if err != nil {
+		logging.FromContext(ctx).Panicf("Failed to create keys %v", err)
+	}
+	data := map[string][]byte{
+		"cert":     certPEM,
+		"private":  privPEM,
+		"public":   pubPEM,
+		"password": []byte(pwd),
+	}
+
+	// Reconcile the "main" secret that's used by Fulcio
+	nsSecret := clientset.CoreV1().Secrets(ns)
+	if err := secret.ReconcileSecret(ctx, *secretName, ns, data, nsSecret); err != nil {
+		logging.FromContext(ctx).Panicf("Failed to reconcile secret %s/%s: %v", ns, *secretName, err)
+	}
+	pubData := map[string][]byte{
+		"cert":   certPEM,
+		"public": pubPEM,
+	}
+	if err := secret.ReconcileSecret(ctx, *pubkeySecretName, ns, pubData, nsSecret); err != nil {
+		logging.FromContext(ctx).Panicf("Failed to reconcile secret %s/%s: %v", ns, *pubkeySecretName, err)
+	}
+}
+
+// createAll creates a password protected keypair, and returns PEM encoded
+// CA Cert, crypto.PublicKey, crypto.PrivateKey, password
+func createAll() ([]byte, []byte, []byte, string, error) {
+	// Generate ECDSA key.
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("failed to generate ecdsa key: %w", err)
+	}
+
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).SetInt64(math.MaxInt64))
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("failed to generate serial Number: %w", err)
+	}
+	rootCA := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization:  []string{*certOrg},
+			Country:       []string{*certCountry},
+			Province:      []string{*certProvince},
+			Locality:      []string{*certLocality},
+			StreetAddress: []string{*certAddr},
+			PostalCode:    []string{*certPostal},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		IsCA:                  true,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		BasicConstraintsValid: true,
+		MaxPathLen:            1,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, rootCA, rootCA, privateKey.Public(), privateKey)
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("failed to create certificate: %w", err)
+	}
+	certPEM := pem.EncodeToMemory(
+		&pem.Block{Type: "CERTIFICATE", Bytes: derBytes},
+	)
+
+	// Encode private key to PKCS #8 ASN.1 PEM.
+	marshalledPrivKey, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("marshal pkcs8 private key: %w", err)
+	}
+	block := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: marshalledPrivKey,
+	}
+
+	// Generate a uuid as a password
+	u := uuid.New()
+	pwd := u.String()
+
+	// Encrypt the pem
+	block, err = pemutil.EncryptPKCS8PrivateKey(rand.Reader, block.Bytes, []byte(pwd), x509.PEMCipherAES256)
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("EncryptPEMBlock failed: %w", err)
+	}
+
+	privPEM := pem.EncodeToMemory(block)
+	if privPEM == nil {
+		return nil, nil, nil, "", fmt.Errorf("EncodeToMemory private key failed: %w", err)
+	}
+
+	marshalledPubKey, err := x509.MarshalPKIXPublicKey(privateKey.Public())
+	if err != nil {
+		return nil, nil, nil, "", fmt.Errorf("failed to unmarshal public key: %w", err)
+	}
+	pubPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: marshalledPubKey,
+		},
+	)
+	if pubPEM == nil {
+		return nil, nil, nil, "", fmt.Errorf("EncodeToMemory public key failed: %w", err)
+	}
+	return certPEM, pubPEM, privPEM, pwd, nil
+}
